8f3587317ce1792decfa3b426195d57fc91a8c4a
chongwei49 <101083554+chongwei49@users.noreply.github.com>
2026-07-13 18:02:34 +0800

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        result = []
+        for i in range(len(temperatures)):
+            actual_day = future_day = i
+            
+            while future_day < len(temperatures):
+                if actual_day == future_day:
+                    future_day += 1
+                    continue
+
+                if temperatures[future_day] > temperatures[actual_day]:
+                    result.append(future_day-actual_day)
+                    break
+                
+                if future_day == len(temperatures)-1:
+                    result.append(0)
+                    break
+
+                future_day += 1
+        
+        result.append(0)
+
+        return result
+        
